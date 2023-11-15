496ebbf1ad031eba8bb3f6d6f11ce7bac8c56751
unknown <tuandht97@gmail.com>
2023-11-15 15:35:50 +0700

    Update code TwoSum

--- src/main/java/org/learn/TwoSum.java
+++ src/main/java/org/learn/TwoSum.java
@@ -1,53 +1,53 @@
 package org.learn;
 
-public class BestTimeToBuyAndSellStock {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-    private static int maxProfit(int[] prices) {
-        int profit = 0;
-        int buyPrice = 0;
-        int sellPrice = 0;
+public class TwoSum {
 
-        for(int i = 0; i < prices.length; i++){
-            if(i == 0) {
-                buyPrice = prices[i];
-                continue;
-            }
-            int tmpProfit = prices[i] - buyPrice;
+    private static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
 
-            if(tmpProfit > profit) {
-                profit = tmpProfit;
-                sellPrice = prices[i];
-            }
-
-            if(buyPrice > prices[i]) {
-                buyPrice = prices[i];
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
             }
         }
 
-        return profit;
+        return result;
     }
 
-    private static int bestSolution(int[] prices) {
-        int lowest = Integer.MAX_VALUE;
-        int gain = 0;
-        int diff = 0;
-
-        for(int i = 0; i < prices.length; i++){
-            if(prices[i] < lowest){
-                lowest = prices[i];
-            }
-            diff = prices[i] - lowest;
-            if(gain < diff){
-                gain = diff;
+    private static int[] bestSolution(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i],i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i){
+                return new int[]{i,map.get(complement)};
             }
         }
-        return gain;
+        //In case there is no solution we'll return null
+        return null;
     }
 
     public static void main(String[] args) {
-        int[] prices = {7,1,5,3,6,4};
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
 
-        System.out.println(maxProfit(prices));
+        int[] nums2 = {3,3};
+        int target2 = 6;
+        System.out.println(Arrays.toString(bestSolution(nums2, target2)));
 
+        int[] nums3 = {3,2,4};
+        int target3 = 6;
+        System.out.println(Arrays.toString(twoSum(nums3, target3)));
     }
 }
