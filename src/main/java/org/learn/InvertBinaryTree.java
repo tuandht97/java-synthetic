7ef5d3ac07f2bc1afc0cc75bbf5db7866518fce5
unknown <tuandht97@gmail.com>
2023-11-16 09:56:17 +0700

    Invert Binary Tree

---
+++ src/main/java/org/learn/InvertBinaryTree.java
@@ -0,0 +1,90 @@
+package org.learn;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class InvertBinaryTree {
+
+    private static TreeNode invertTree(TreeNode root) {
+        recurseInvert(root);
+        return root;
+    }
+
+    private static void recurseInvert(TreeNode node){
+        if(node == null){
+            return;
+        }
+        TreeNode left = node.left;
+        TreeNode right = node.right;
+        node.left = right;
+        node.right = left;
+        recurseInvert(node.left);
+        recurseInvert(node.right);
+    }
+
+    private static void preorderTraversal(TreeNode node) {
+        if (node == null)
+            return;
+
+        System.out.print(node.val + " ");
+        preorderTraversal(node.left);
+        preorderTraversal(node.right);
+    }
+
+    private static void inorderTraversal(TreeNode node) {
+        if (node == null)
+            return;
+
+        inorderTraversal(node.left);
+        System.out.print(node.val + " ");
+        inorderTraversal(node.right);
+    }
+
+    private static void postorderTraversal(TreeNode node) {
+        if (node == null)
+            return;
+
+        postorderTraversal(node.left);
+        postorderTraversal(node.right);
+        System.out.print(node.val + " ");
+    }
+
+    public static void main(String[] args) {
+        TreeNode tree = new TreeNode(4);
+        tree.left = new TreeNode(2);
+        tree.left.left = new TreeNode(1);
+        tree.left.right = new TreeNode(3);
+        tree.right = new TreeNode(7);
+        tree.right.left = new TreeNode(6);
+        tree.right.right = new TreeNode(9);
+
+        System.out.println("Preorder Traversal:");
+        preorderTraversal(tree);
+
+        System.out.println("\nInorder Traversal:");
+        inorderTraversal(tree);
+
+        System.out.println("\nPostorder Traversal:");
+        postorderTraversal(tree);
+
+        TreeNode invertTree = invertTree(tree);
+        System.out.println("\nInvert Tree:");
+        preorderTraversal(invertTree);
+    }
+}
